1eee8168aa68fa8a10d26b0faac408f3ca43bb8a
elimscouts-lgtm <elim.scouts@gmail.com>
2026-06-11 10:07:34 -0700

    adding comments

--- chapter_02_work/chapter_02_exercises/famous_quote.py
+++ chapter_02_work/chapter_02_exercises/famous_quote.py
@@ -1,4 +1,7 @@
+# The quote and the person who said it
 quote = '"If you know the enemy and know yourself, you need not fear the result of a hundred battles"'
 famous_person = "sun tzu"
+
+#putting it all together and printing
 final_message = f"{famous_person.title()} once said, {quote}."
 print(final_message)
