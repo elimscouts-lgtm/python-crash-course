aa8c0627dd7e8a113b67e7597aabb04477738fed
elimscouts-lgtm <elim.scouts@gmail.com>
2026-06-11 09:16:42 -0700

    representing the whole message as a string before printing

--- famous_quote.py
+++ famous_quote.py
@@ -1,3 +1,4 @@
 quote = '"If you know the enemy and know yourself, you need not fear the result of a hundred battles"'
 famous_person = "sun tzu"
-print(f"{famous_person.title()} once said, {quote}.")
+final_message = f"{famous_person.title()} once said, {quote}."
+print(final_message)
