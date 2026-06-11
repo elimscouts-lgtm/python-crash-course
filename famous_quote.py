c16d8877ed507aa176774aa0a350c8a867583e0c
elimscouts-lgtm <elim.scouts@gmail.com>
2026-06-11 09:14:21 -0700

    Printing a famous quote using an f-string

---
+++ famous_quote.py
@@ -0,0 +1,3 @@
+quote = '"If you know the enemy and know yourself, you need not fear the result of a hundred battles"'
+famous_person = "sun tzu"
+print(f"{famous_person.title()} once said, {quote}.")
