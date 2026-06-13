e4c24caa3c80ad4805d88d745b8c2abd014610d0
elimscouts-lgtm <elim.scouts@gmail.com>
2026-06-12 20:56:44 -0700

    printing a message using elements from a list

---
+++ chapter_03_work/chapter_02_exercises/transportation_i_like.py
@@ -0,0 +1,5 @@
+vehicles = ["porsche", "plane", "motorcycle"]
+print(f"I would like to own a {vehicles[0]}.")
+print(f"I would like to own a {vehicles[1]}.")
+print(f"I would like to own a {vehicles[2]}.")
+print(f"I would like to own a {vehicles[-1]}.")
