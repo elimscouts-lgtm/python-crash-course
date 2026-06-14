9e7e2acfee8fa35a8333a0f0b02cdea284f8acd2
elimscouts-lgtm <elim.scouts@gmail.com>
2026-06-14 15:25:26 -0700

    changing the list by removing and inserting variables

--- chapter_03_work/chapter_02_exercises/guest_list.py
+++ chapter_03_work/chapter_02_exercises/guest_list.py
@@ -2,3 +2,13 @@ invite_list = ["archimedes", "jim parsons", "ari gibson"]
 print(f"You are invited to dinner at my house, {invite_list[0].title()}.")
 print(f"You are invited to dinner at my house, {invite_list[1].title()}.")
 print(f"You are invited to dinner at my house, {invite_list[2].title()}.")
+
+print(f"{invite_list[2].title()} can't make it!")
+
+del invite_list[2]
+invite_list.insert(2, "keanu reeves")
+
+print(f"You are invited to dinner at my house, {invite_list[0].title()}.")
+print(f"You are invited to dinner at my house, {invite_list[1].title()}.")
+print(f"You are invited to dinner at my house, {invite_list[2].title()}.")
+
