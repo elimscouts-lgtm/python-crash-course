c4a541abd5da639ca4444dc4842e99faeef53d6b
elimscouts-lgtm <elim.scouts@gmail.com>
2026-06-14 15:20:18 -0700

    Printing a message inviting people to dinner at my house using a list.

---
+++ chapter_03_work/chapter_02_exercises/guest_list.py
@@ -0,0 +1,4 @@
+invite_list = ["archimedes", "jim parsons", "ari gibson"]
+print(f"You are invited to dinner at my house, {invite_list[0].title()}.")
+print(f"You are invited to dinner at my house, {invite_list[1].title()}.")
+print(f"You are invited to dinner at my house, {invite_list[2].title()}.")
