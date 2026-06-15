7c582ebe51da2bafce18a40a9f79ce9189a4b07a
elimscouts-lgtm <elim.scouts@gmail.com>
2026-06-14 20:35:15 -0700

    organizing the for loop and adding a line to be executed after the for loop

--- chapter_04_work/magicians.py
+++ chapter_04_work/magicians.py
@@ -1,3 +1,6 @@
 magicians = ["paul", "ian", "bill"]
 for magician in magicians:
     print(f"{magician.title()}, that was a great trick!")
+    print(f"I cant wait to see another trick, {magician.title()}")
+    print()
+print("Thank you everyone, that was a great show!")
