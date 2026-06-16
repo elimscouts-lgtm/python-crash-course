109a760eec4abee49373dbf70bb4daeb9e1bde6d
elimscouts-lgtm <elim.scouts@gmail.com>
2026-06-16 09:40:05 -0700

    adding slices to print the middle, start and end of the list.

--- chapter_04_work/chapter_04_exercises/one_million.py
+++ chapter_04_work/chapter_04_exercises/one_million.py
@@ -2,3 +2,6 @@ million = [value * 1 for value in range(1, 1000001)]
 print(min(million))
 print(max(million))
 print(sum(million))
+print(million[:3])
+print(million[-3:])
+print(million[len(million)//2-1:len(million)//2+1])
