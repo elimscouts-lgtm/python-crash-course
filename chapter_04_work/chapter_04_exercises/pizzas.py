82d756ec128c76fe60922c5b7e155b22ec8e97f8
elimscouts-lgtm <elim.scouts@gmail.com>
2026-06-16 09:45:35 -0700

    copying the list to make another list and then modifying that list

--- chapter_04_work/chapter_04_exercises/pizzas.py
+++ chapter_04_work/chapter_04_exercises/pizzas.py
@@ -2,3 +2,7 @@ pizzas = ["pepperoni", "cheese", "meat lover's"]
 for pizza in pizzas:
     print(f"A type of pizza that I like is {pizza}.")
 print("Pizza is a food that I like.")
+friend_pizzas = pizzas[:]
+friend_pizzas.append("pineaple")
+for friend_pizza in friend_pizzas:
+    print(f"A type of pizza that my friend likes is {friend_pizza}.")
