48aba08166bd66469ad86bf5ee1a927406670509
elimscouts-lgtm <elim.scouts@gmail.com>
2026-07-15 11:12:03 -0700

    printing stages of life based on age assigned

---
+++ Chapter_05_Work/Chapter_05_Exercises/stage_of_life.py
@@ -0,0 +1,7 @@
+age = 80
+if age <=2: print("baby")
+elif age <=12: print("child")
+elif age <=17: print("teenager")
+elif age <=18: print("adult")
+elif age <=60: print("senior")
+else: print("very old")
