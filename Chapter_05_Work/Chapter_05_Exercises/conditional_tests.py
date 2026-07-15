115d583b18878d4d3046a1452e67870482ea55a2
elimscouts-lgtm <elim.scouts@gmail.com>
2026-07-15 10:44:21 -0700

    conditional tests for strings and numbers

---
+++ Chapter_05_Work/Chapter_05_Exercises/conditional_tests.py
@@ -0,0 +1,17 @@
+car = "porsche"
+print("is car == subaru? I think not.")
+print(car == "subaru")
+
+print(f"is car == porsche? I think {car == "porsche"}.")
+
+number = 4
+print(f"{number}")
+if number >= 3:
+    print("more than or equal to 3")
+else:
+    print("less than 3")
+
+if number <= 2:
+    print("less than or equal to 2")
+else:
+    print("more than 2")
---
+++ Chapter_05_Work/toppings.py
@@ -0,0 +1,3 @@
+topping = "cheese"
+if topping != "anchovies" and topping == "cheese":
+    print("Hold the anchovies! I like cheese!")
